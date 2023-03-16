23c40c9b0866fff2399a19961977603c22fc3289
Alexandrov <alex.44747.alex@gmail.com>
2023-03-16 15:23:01 +0300

    Домашка 5  ветка hW5 ,домашка готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,140 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int clientOs = 0;
+        int clientAndroid = 1;
+        if (clientOs == 0) {
+            System.out.println("Установите версию для IOS по ссылке");
+        } else {
+            System.out.println("Такой команды для IOS нет");
+        }
+
+        if (clientAndroid == 1) {
+            System.out.println("Установите версию для Android по ссылке");
+        } else {
+            System.out.println("Такой команды Android нет");
+        }
+
+        System.out.println("Задача 2");
+
+        int clientDeviceYear = 2014;
+
+        if (clientOs == 0) {
+            if (clientDeviceYear > 2015) {
+                System.out.println("Установите версию для IOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+
+            }
+
+        }if (clientAndroid ==1){
+            if (clientDeviceYear >2015){
+                System.out.println("Установите версию для Android по ссылке");
+            }else {
+                System.out.println("Установите облегченную версию Android по ссылке");
+            }
+        }
+
+        System.out.println("Задача 3");
+
+        int ear = 2025;
+        int highYear = ear % 4; // Високосный год 29 февраля раз в 4 года ,год делим на 4
+        System.out.println("Остаток от деления "+highYear);
+
+        switch (ear){
+            case 2020:
+                System.out.println("Високосный");
+                break;
+            case 2021:
+                System.out.println("Не високосный");
+                break;
+            case 2022:
+                System.out.println("Не високосный");
+                break;
+            case 2023:
+                System.out.println("Не високосный");
+                break;
+            case 2024:
+                System.out.println("Високосный");
+                break;
+            default:
+                System.out.println("НЕ понятно");
+
+                System.out.println("Задача 4");
+
+                int deliveryDistance = 19;
+                int daysForDelivery ;
+
+
+                if (deliveryDistance < 20){
+                    System.out.println("потребуется дней: 1");
+                }else if (deliveryDistance > 20 && deliveryDistance <60){
+                    System.out.println("Потребуется дней:2");
+                }else if (deliveryDistance >60 && deliveryDistance <= 100){
+                    System.out.println("Потребуется дней:3");
+                }else{
+                    System.out.println("На данное расстояние дотавка не осуществляется");
+                }
+
+                System.out.println("Задача 5");
+
+               int monthNumber = 11;
+
+                switch (monthNumber){
+                    case 1:
+                        System.out.println("1 месяц в году январь,зимний месяц");
+                        break;
+                    case 2:
+                        System.out.println("2 месяц в году февраль,зимний месяц");
+                        break;
+                    case 3:
+                        System.out.println("3 месяц в году март,весенний месяц");
+                        break;
+                    case 4:
+                        System.out.println("4 месяц в году апрель,весенний месяц");
+                        break;
+                    case 5:
+                        System.out.println("5 месяц в году май,весенний месяц");
+                        break;
+                    case 6:
+                        System.out.println("6 месяц в году июнь,летний месяц");
+                        break;
+                    case 7:
+                        System.out.println("7 месяц в году июль,летний месяц");
+                        break;
+                    case 8:
+                        System.out.println("8 месяц в году август,летний месяц");
+                        break;
+                    case 9:
+                        System.out.println("9 месяц в году сентябрь,осенний месяц");
+                        break;
+                    case 10:
+                        System.out.println("10 месяц в году октябрь,осенний месяц");
+                        break;
+                    case 11:
+                        System.out.println("11 месяц в году ноябрь,осенний месяц");
+                        break;
+                    case 12:
+                        System.out.println("12 месяц в году декабрь,зимний месяц");
+                        break;
+
+                    default:
+                        System.out.println("Номер месяца больше 12");
+
+
+                }
+
+
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
